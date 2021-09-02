c88a770af7cc4be0b44c26441a178b4638811c53
Jin-Zhu Yu <yujinzhu88@gmail.com>
2021-09-02 17:42:59 -0400

    Update infer_topology_from_steady_states.jl

--- src/infer_topology_from_steady_states.jl
+++ src/infer_topology_from_steady_states.jl
@@ -7,38 +7,44 @@ using JuMP, Gurobi
 function find_topology(k, fs, gs)
 
     ### find network topology given degree sequence, steady states, and network dynamics
-    # xs: list of steady states; k: list of degree sequence; fs, gs: function values for self dynamics and interaction between adjacent nodes at steady states
+    # ks: list of degree sequence; fs, gs: function values for self dynamics and interaction between adjacent nodes at steady states
     ###
 
     # initialize model
-    mp = Model(optimizer_with_attributes(() -> Gurobi.Optimizer(), "OutputFlag" => 0));
+    model = Model(optimizer_with_attributes(() -> Gurobi.Optimizer(), "OutputFlag" => 0));
 
     # define variables
     n_node = length(k);
-    @variable(mp, Adj[i in 1:n_node, j in 1:n_node], Bin);
-    @variable(mp, aux_var[i in 1:n_node]);
+    @variable(model, Adj[i in 1:n_node, j in 1:n_node], Bin);
+    @variable(model, aux_var[i in 1:n_node]);
 
     # constraints
     # symmetry
-    @constraint(mp, symmetry[i in 1:n_node, j in 1:n_node], Adj[i,j] == Adj[j,i]);
+    @constraint(model, symmetry[i in 1:n_node, j in 1:n_node], Adj[i,j] == Adj[j,i]);
     # degree sequence
-    @constraint(mp, degree[i in 1:n_node], sum(Adj[i,j] for j in 1:n_node) == k[i]);
+    @constraint(model, degree[i in 1:n_node], sum(Adj[i,j] for j in 1:n_node) == k[i]);
 
     # objective function
-    @expression(mp, cost[i in 1:n_node], fs[i] + sum(Adj[i,j]*gs[i,j] for j in 1:n_node) );
+    @expression(model, cost[i in 1:n_node], fs[i] + sum(Adj[i,j]*gs[i,j] for j in 1:n_node) );
     # linearize the absolute term in the obj
-    @constraint(mp, LB1[i in 1:n_node], aux_var[i] >= cost[i]);
-    @constraint(mp, LB2[i in 1:n_node], aux_var[i] >= -cost[i]);
+    @constraint(model, LB1[i in 1:n_node], aux_var[i] >= cost[i]);
+    @constraint(model, LB2[i in 1:n_node], aux_var[i] >= -cost[i]);
 
-    @objective(mp, Min, sum(aux_var[i] for i in 1:n_node) );
+    @objective(model, Min, sum(aux_var[i] for i in 1:n_node) );
 
+    # solve the model
     t0 = time();
-    optimize!(mp);
+    optimize!(model);
     t1 = time();
 
-    t_sol = t1 - t0
+    if termination_status(m) != MOI.OPTIMAL
+        @warn("===== Final solution not optimal =====")
+        return
+    end
 
-    Adj = mp[:Adj];
+    # return solution time and solution
+    t_sol = t1 - t0
+    Adj = model[:Adj];
 
     return Adj, t_sol;
 end
