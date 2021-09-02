e3639f39dfee9997cf530027912f0b06483b9fd6
Jin-Zhu Yu <yujinzhu88@gmail.com>
2021-09-02 17:35:09 -0400

    Initial commit

---
+++ data/utils.py
@@ -0,0 +1,68 @@
+import pickle
+import numpy as np
+
+def load_synthetic_data(dyn, net):
+    '''
+    Load synthetic networks
+    '''
+    fnm = 'net-{}.pkl'.format(net)
+    with open(fnm,'rb') as file:
+        dat=pickle.load(file)
+    G, ks, A_real, A_hh = dat['G'], dat['ks'], dat['A_real'], dat['A_hh']
+
+    fnm = 'xs-{0}-{1}.pkl'.format(dyn,net)
+    with open(fnm,'rb') as file:
+        xs = pickle.load(file)
+
+    fs,gs=interactions(dyn, xs, ks)
+    return G, xs, ks, A_real, A_hh, fs, gs
+
+
+def interactions(dyn,xs,ks=None):
+    '''
+    Obtain the interaction terms (self or mutual interactions)
+
+    Parameters:
+    ----------
+    dyn:        dynamics
+    xs:         steady states
+    ks:         degrees
+
+    Returns:
+    --------
+    self and mutual interaction terms
+    '''
+    N = len(xs)
+    if dyn == 'ecology':
+        B, C, D, E, H, K = 0.1, 1, 5, 0.9, 0.1, 5
+        fs = B + xs * (1 - xs / K) * (xs / C - 1)
+        gs = np.array([[xs[i] * xs[j] / (D + E * xs[i] + H * xs[j]) for j in range(N)] for i in range(N)])
+    elif dyn == 'regulatory':
+        B, f, h = 1, 1, 2
+        fs = -B * xs ** f
+        gs = np.repeat([[xs[j] ** h / (xs[j] ** h + 1) for j in range(N)]], [N], axis=0)
+    elif dyn == 'epidemic':
+        R, B = 1, 2
+        fs = -B * xs
+        gs = np.array([[R * (1 - xs[i]) * xs[j] for j in range(N)] for i in range(N)])
+    elif dyn == 'pagerank':
+        alpha = 0.85
+        fs = (1-alpha)/N - xs
+        gs = alpha*np.array([[xs[j]/ks[j] for j in range(N)] for i in range(N)])
+
+    for i in range(N):
+        gs[i, i] = 0
+    return fs, gs
+
+dyn='ecology'
+real = False
+
+net = 'BA-n{0}-m{1}-{2}'.format(100,4,99)
+
+G, xs_real, ks, A_real, A_hh, fs, gs = load_synthetic_data(dyn, net)
+
+
+# import os
+# os.chdir('C:/code/network_reconstruction/data')
+
+
---
+++ src/infer_topology_from_steady_states.jl
@@ -0,0 +1,48 @@
+# construct the extensive formulation
+
+using JuMP, Gurobi
+# using Statistics, CSV, DataFrames;
+# using Random;
+
+function find_topology(k, fs, gs)
+
+    ### find network topology given degree sequence, steady states, and network dynamics
+    # xs: list of steady states; k: list of degree sequence; fs, gs: function values for self dynamics and interaction between adjacent nodes at steady states
+    ###
+
+    # initialize model
+    mp = Model(optimizer_with_attributes(() -> Gurobi.Optimizer(), "OutputFlag" => 0));
+
+    # define variables
+    n_node = length(k);
+    @variable(mp, Adj[i in 1:n_node, j in 1:n_node], Bin);
+    @variable(mp, aux_var[i in 1:n_node]);
+
+    # constraints
+    # symmetry
+    @constraint(mp, symmetry[i in 1:n_node, j in 1:n_node], Adj[i,j] == Adj[j,i]);
+    # degree sequence
+    @constraint(mp, degree[i in 1:n_node], sum(Adj[i,j] for j in 1:n_node) == k[i]);
+
+    # objective function
+    @expression(mp, cost[i in 1:n_node], fs[i] + sum(Adj[i,j]*gs[i,j] for j in 1:n_node) );
+    # linearize the absolute term in the obj
+    @constraint(mp, LB1[i in 1:n_node], aux_var[i] >= cost[i]);
+    @constraint(mp, LB2[i in 1:n_node], aux_var[i] >= -cost[i]);
+
+    @objective(mp, Min, sum(aux_var[i] for i in 1:n_node) );
+
+    optimize!(mp)
+
+    solution_time = 
+
+    Adj = 
+
+    return Adj, solution_time;
+end
+
+# test
+
+
+
+
